9734491dda2c7b1cf44926c2344137ad6da1708d
yoshiyasugimoto <gabesaiti@gmail.com>
2020-08-09 13:12:45 +0900

    used the React  Hooksxxx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>Thsi is a template for React app.</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+
+  const reset = () => setCount(0);
+  const double = () => setCount(count * 2);
+  // const divide3 = () =>
+  //   setCount((previousCount) => {
+  //     if (previousCount % 3 == 0) {
+  //       return previousCount / 3;
+  //     } else {
+  //       return previousCount;
+  //     }
+  //   });
+  const divide3 = (previousCount) => {
+    return previousCount % 3 === 0 ? previousCount / 3 : previousCount;
+  };
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>double</button>
+        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
